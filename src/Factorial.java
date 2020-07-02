23f084e0790779de54f0472e1cbca258342e9a8c
DLunaG <LunaLunae91@gmail.com>
2020-07-02 19:35:45 +0200

    Creación de una clase java de un problema factorial

---
+++ src/Factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+        public static void main(String[] args) {
+            int contador = 1;
+            int resultado = 1;
+            int valor;
+
+            Scanner s = new Scanner(System.in);
+            System.out.println("Introduce el nº del factorial que quieras hayar: ");
+            valor = s.nextInt();
+
+            while(contador<=valor){
+                resultado = resultado * contador;
+                contador++;
+            }
+
+            System.out.println("El factorial de "+valor+" es: "+resultado);
+
+
+        }
+}
+
